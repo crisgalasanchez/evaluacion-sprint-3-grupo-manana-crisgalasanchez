4d9d6d785655b8512601f55083f6866c6f56d1a3
Cris <crisgalasanchez@gmail.com>
2018-01-07 15:56:51 +0100

    ajax to create reasons

--- js/main.min.js
+++ js/main.min.js
@@ -20,6 +20,25 @@ arribaButton.onclick = function(){
     window.scrollTo(0,0);
 }
 //AJAX
+var moreReasons = document.getElementById('more-reasons');
+moreReasons.addEventListener('click', requestReasons);
+
+function createReason(element){
+	var explainReasons = document.getElementById('explain-reasons');
+	var divItemReason = document.createElement('div');
+	divItemReason.className += " item-reason";
+	var titleItemReason = document.createElement('h3');
+	titleItemReason.className += " pretitle";
+	titleItemReason.innerHTML=element.title;
+	var paragraphItemReason = document.createElement('p');
+	paragraphItemReason.className += " text-reasons";
+	paragraphItemReason.innerHTML=element.description;
+
+	divItemReason.appendChild(titleItemReason);
+	divItemReason.appendChild(paragraphItemReason);
+	explainReasons.appendChild(divItemReason);
+}
+
 function requestReasons() {
 	var request = new XMLHttpRequest();
 	request.open('GET', 'https://three-random-reasons-izwfjpgbqm.now.sh/');
@@ -31,6 +50,12 @@ function requestReasons() {
 
 		//TODO cargar los datos de la respuesta en el html ... a las propiedades de responseJSON se accede como todos los objetos.
 		//Ejemplo: JSONresponse.reasons
+
+		var reasons = responseJSON.reasons;
+		for(var i=0; i < reasons.length; i++){
+			var reasonItem = reasons[i];
+			createReason(reasonItem);
+		}
 	}
 
 	request.send();
